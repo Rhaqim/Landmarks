391cf0e534b5309e70664378fec1478d70e01090
Rhaqim <anusiemj@gmail.com>
2024-01-08 22:12:05 +0100

    Using tabs and adding the featured section, horizontal scrolling and a featured tab

---
+++ dejoapp/Views/Categories/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  dejoapp
+//
+//  Created by Anusiem John-Franklin on 08/01/2024.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
---
+++ dejoapp/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  dejoapp
+//
+//  Created by Anusiem John-Franklin on 08/01/2024.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
