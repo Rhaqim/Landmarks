ebb33d77ab32c454c054bee00c15eca83f858b0e
Rhaqim <anusiemj@gmail.com>
2024-01-09 02:52:30 +0100

    WWorking with the user profile and editing the profile, using a sheet for modal

---
+++ dejoapp/Model/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  dejoapp
+//
+//  Created by Anusiem John-Franklin on 09/01/2024.
+//
+
+import Foundation
---
+++ dejoapp/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,18 @@
+//
+//  HikeBadge.swift
+//  dejoapp
+//
+//  Created by Anusiem John-Franklin on 09/01/2024.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HikeBadge()
+}
