321376c994267e6d28b29c66dc20480a26e935b3
Rhaqim <anusiemj@gmail.com>
2024-01-09 11:18:56 +0100

    editing the profile

--- dejoapp/Views/Profiles/ProfileHost.swift
+++ dejoapp/Views/Profiles/ProfileHost.swift
@@ -9,16 +9,43 @@ import SwiftUI
 
 struct ProfileHost: View {
     
-    @State private var defaultProfile = Profile.default
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
+    
+    @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            ProfileSummary(profile: defaultProfile)
+            
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                
+                Spacer()
+                
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ProfileHost()
+    ProfileHost().environment(ModelData())
 }
