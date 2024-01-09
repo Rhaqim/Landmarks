ae47836e123641cd1d5e0238022eb668ab085d93
Rhaqim <anusiemj@gmail.com>
2024-01-09 11:18:33 +0100

    Editing changes in the user profile

---
+++ dejoapp/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor.swift
+//  dejoapp
+//
+//  Created by Anusiem John-Franklin on 09/01/2024.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}
